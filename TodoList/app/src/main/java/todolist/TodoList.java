0f83d2ca0ae79ea1c98fe41990e67cc67b46b281
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-09-06 16:25:03 +0100

    passes second test with items added to list

--- TodoList/app/src/main/java/todolist/TodoList.java
+++ TodoList/app/src/main/java/todolist/TodoList.java
@@ -5,10 +5,9 @@ import java.util.List;
 
 public class TodoList {
     List<String> tasks = new ArrayList<String>();
-//    public void addTask(String task) {
-//        tasks.add(task);
-//    }
-
+    public void addTask(String task) {
+        tasks.add(task);
+    }
     public List<String> getTasks() {
         return tasks;
     }
--- TodoList/app/src/test/java/todolist/TodoListTest.java
+++ TodoList/app/src/test/java/todolist/TodoListTest.java
@@ -5,19 +5,20 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TodoListTest {
-        @Test public void testGetTasksNoitems() {
+    @Test public void testGetTasksNoItems() {
         TodoList list = new TodoList();
         List<String> res = new ArrayList<String>();
         assertEquals("should return array of tasks", res, list.getTasks());
     }
-//    @Test public void testGetTasksWithItemsAdded() {
-//        TodoList list = new TodoList();
-//        list.addTask("clean");
-//        list.addTask("cook");
-//        String[] res = {"clean", "cook"};
-//        assertEquals("should return array of tasks", res, list.getTasks());
-//    }
+    @Test public void testGetTasksWithItemsAdded() {
+        TodoList list = new TodoList();
+        list.addTask("clean");
+        list.addTask("cook");
+        List<String> res = Arrays.asList("clean", "cook");
+        assertEquals("should return array of tasks", res, list.getTasks());
+    }
 }
